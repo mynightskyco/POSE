4d67028a889adfd4c127e18bf6ff79d071447789
Dylan Wagner <vstrideultimate@gmail.com>
2019-03-04 12:53:17 -0500

    added body code

--- src/bodies.rs
+++ src/bodies.rs
@@ -10,8 +10,34 @@ pub struct Debris{
     pub x_vel: f64,
     pub y_vel: f64,
     pub z_vel: f64
+    // Add more
 }
 
 impl Simobj for Debris {}
 
 pub type SimobjT = Box<dyn Simobj>;
+
+pub enum Solarobj{
+    Sun,
+    Mercury,
+    Venus,
+    Earth,
+    Mars,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune
+}
+
+pub struct LargeBody{
+    pub solartype: Solarobj,
+    pub mass: f64, // kg
+    // meters
+    pub radius: f64,
+    pub x_dis: f64,
+    pub y_dis: f64,
+    pub z_dis: f64,
+    pub x_vel: f64,
+    pub y_vel: f64,
+    pub z_vel: f64    
+}
