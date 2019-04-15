b5b2c5f7a801b7fa9a3fbe372af2e761277460b1
cory_jay44 <liftid44@gmail.com>
2019-04-15 00:58:04 -0400

    Made generation of debris and satellites more realistic

--- aux/generate.py
+++ aux/generate.py
@@ -4,26 +4,65 @@ import sys
 import getopt
 import uuid
 import datetime
+import math
+
+# Better Sphereical Random Point Function
+
+radus_of_earth = 6378100  # in meters
+
+
+def getPoint():
+    u = random.random()
+    v = random.random()
+    theta = u * 2.0 * math.pi
+    phi = math.acos(2.0 * v - 1.0)
+    sin_theta = math.sin(theta)
+    cos_theta = math.cos(theta)
+    sin_phi = math.sin(phi)
+    cos_phi = math.cos(phi)
+    x = radus_of_earth * sin_phi * cos_theta
+    y = radus_of_earth * sin_phi * sin_theta
+    z = radus_of_earth * cos_phi
+    return x, y, z
+
+
+# Define Low Orbit Statistics(Circular Orbit) https://en.wikipedia.org/wiki/Orbital_speed
+
+min_low_orbit = 6578100.0  # m 200,000 min w/o radius
+max_low_orbit = 8378100.0  # m 2,000,000 max w/o radius
+
+minimal_velocity = 6900.0  # m/s
+max_velocity = 7800.0  # m/s
+
+# Linear Scale, might want to switch to an exponential scale
+
+
+def scaling_velocity(x: float) -> float:
+    return (((max_velocity-minimal_velocity)*(x-min_low_orbit))/(max_low_orbit - min_low_orbit)) + minimal_velocity
+
 
 data = {"date": "", "debris": [], "spacecraft": []}
 ref = "data/"
 
 """make distance respect earth's radius, make velocity believable"""
 
-def randNum() -> float:
-    return random.uniform(0.0, 100.0)
+
+def rand_orbit() -> float:
+    return random.uniform(min_low_orbit, max_low_orbit)
 
 
 def create_spacecraft() -> dict:
     spacecraft = {}
     # spacecraft["id"] = uuid.uuid4().int & (1 >> 32)-1
     spacecraft["id"] = random.getrandbits(32)
-    spacecraft["x_dis"] = randNum()
-    spacecraft["y_dis"] = randNum()
-    spacecraft["z_dis"] = randNum()
-    spacecraft["x_vel"] = randNum()
-    spacecraft["y_vel"] = randNum()
-    spacecraft["z_vel"] = randNum()
+    rand_orbital = rand_orbit()
+    rand_x, rand_y, rand_z = getPoint()
+    spacecraft["x_dis"] = rand_x + rand_orbital
+    spacecraft["y_dis"] = rand_y + rand_orbital
+    spacecraft["z_dis"] = rand_z + rand_orbital
+    spacecraft["x_vel"] = scaling_velocity(rand_x + rand_orbital)
+    spacecraft["y_vel"] = scaling_velocity(rand_y + rand_orbital)
+    spacecraft["z_vel"] = scaling_velocity(rand_z + rand_orbital)
     return spacecraft
 
 
@@ -31,12 +70,14 @@ def create_debris() -> dict:
     debris = {}
     # debris["id"] = uuid.uuid4().int & (1 >> 32)-1
     debris["id"] = random.getrandbits(32)
-    debris["x_dis"] = randNum()
-    debris["y_dis"] = randNum()
-    debris["z_dis"] = randNum()
-    debris["x_vel"] = randNum()
-    debris["y_vel"] = randNum()
-    debris["z_vel"] = randNum()
+    rand_orbital = rand_orbit()
+    rand_x, rand_y, rand_z = getPoint()
+    debris["x_dis"] = rand_x + rand_orbital
+    debris["y_dis"] = rand_y + rand_orbital
+    debris["z_dis"] = rand_z + rand_orbital
+    debris["x_vel"] = scaling_velocity(rand_x + rand_orbital)
+    debris["y_vel"] = scaling_velocity(rand_y + rand_orbital)
+    debris["z_vel"] = scaling_velocity(rand_z + rand_orbital)
     return debris
 
 
